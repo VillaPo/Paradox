e6ffaa2f32147ce1bb0ab1568f4dc22fc804ed69
artiq <art.iq@hotmail.com>
2020-06-16 19:57:59 +0300

    комментарии

--- Two_Envelops/Program.cs
+++ Two_Envelops/Program.cs
@@ -6,7 +6,7 @@ namespace Two_Envelops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");  // выкинуть нахрен
         }
     }
 }
