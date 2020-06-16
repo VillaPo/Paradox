1af9f1c7566b5d98a1dbc23645421ee02460885b
artiq <art.iq@hotmail.com>
2020-06-16 19:33:22 +0300

    Добавьте файлы проекта.

---
+++ Two_Envelops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Two_Envelops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
